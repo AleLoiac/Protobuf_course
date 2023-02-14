3a73a133746824afac458f02c42b991ac431a86a
AleLoiac <alessandroloiac@gmail.com>
2023-02-14 16:20:04 +0100

    14/02

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"Protobuf/src/simple"
+	"fmt"
+)
 
+func doSimple() {
+	sm := simple.SimpleMessage{
+		Id:         12345,
+		IsSimple:   true,
+		Name:       "Sample message",
+		SampleList: []int32{1, 3, 5, 7},
+	}
+	fmt.Println(sm)
+
+	fmt.Println(sm.GetId())
+}
+
+func main() {
+	doSimple()
 }
